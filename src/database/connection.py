577e7fc108620f4e962cc050132f0328cd227e8d
henry <misty0314@naver.com>
2025-08-30 00:43:37 +0900

    데이터베이스 연결
    [라이브러리]
    - sqlalchemy: 관계형 데이터베이스를 사용하기 위한 High-level 인터페이스를 제공하는 Python 라이브러리
    - pymysql: Python과 MySQL을 연동할 때 사용되는 드라이버
    - cryptography: pymysql을 통해서 MySQL에 접속 할 때 인증이나 암호 관련된 처리를 해주는 라이브러리

---
+++ src/database/connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3306/todos"
+
+engine = create_engine(DATABASE_URL, echo=True)
+# echo=True: 쿼리가 대신 처리될 때 어떤 SQL이 사용되었는지 사용되는 시점에 SQL을 출력해주는 옵션
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)

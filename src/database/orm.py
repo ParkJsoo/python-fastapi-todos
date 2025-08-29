6a90e1bb8c889f8febed118b4ac5a365892aed91
henry <misty0314@naver.com>
2025-08-30 01:02:51 +0900

    __repr__ 설명 추가

--- src/database/orm.py
+++ src/database/orm.py
@@ -12,6 +12,9 @@ class ToDo(Base):
     is_done = Column(Boolean, nullable=False)
 
     # 데이터를 보기 쉽게 하기 위해서 어떤 객체가 출력되는지 보기 쉽게 하기 위해서 Python 클래스의 repr 매직 메소드 오버라이딩
+    # __repr__ 매직 메소드: 파이썬 객체를 문자열로 표현할 때 호출되는 특수 메소드
+    # 기본적으로는 <클래스이름 object at 메모리주소> 형태의 다소 불친절한 문자열을 반환
+    # 단순히 “메모리 주소”만 보여주는 기본 출력 대신, 객체의 중요한 속성을 드러내는 표현으로 바꾸기 위해 오버라이드
     def __repr__(self):
         return f"ToDo(id={self.id}, contents='{self.contents}', is_done={self.is_done})"
     # f-string: 문자열 안에서 변수나 표현식을 간결하게 삽입할 수 있는 문법

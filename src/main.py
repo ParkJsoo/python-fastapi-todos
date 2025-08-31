74da35dbc20bd4ca20040d58d1ca52123b5d39e6
henry <misty0314@naver.com>
2025-08-31 20:19:21 +0900

    ORM 적용 - HTTP Response 처리

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm.session import Session
 from database.connection import get_db
 from database.orm import ToDo
 from database.repository import get_todos
+from schema.response import ListToDoResponse, ToDoSchema
 
 app = FastAPI()
 
@@ -36,13 +37,18 @@ todo_data = {
 def get_todos_handler(
         order: str | None = None,
         session: Session = Depends(get_db),
-):
+) -> ListToDoResponse:
     todos: List[ToDo] = get_todos(session=session)
 
     if order and order == "DESC":
-        return todos[::-1]
+        return ListToDoResponse(
+            todos=[ToDoSchema.from_orm(todo) for todo in todos[::-1]]
+        )
 
-    return todos
+    return ListToDoResponse(
+        todos=[ToDoSchema.from_orm(todo) for todo in todos]
+        # Python의 List Comprehension 문법: list를 짧고 간결하게 생성하는 방법
+    )
 
 @app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(todo_id: int):
